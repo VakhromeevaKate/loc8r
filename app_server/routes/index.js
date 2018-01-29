dad2c465f14f615ba8d278dfef8a41dbf9992c0a
WEB-PC6\User <vakhromeevae@gmail.com>
2018-01-29 17:07:40 +0300

    splitting route and controller part 2

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var homepageController = function(req, res, next) {
-    res.render('index', { title: 'Express' });
-};
-/* GET home page. */
-router.get('/', homepageController);
+var ctrlMain = require('../controllers/main');
 
+/* GET home page. */
+router.get('/', ctrlMain.index);
 module.exports = router;
 
 
